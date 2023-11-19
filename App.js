c114f243ff920dde511ee30ded5f62d863570280
afika <afikashwashwa@gmail.com>
2023-11-19 21:06:06 +0200

    ...

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 
+
 export default function App() {
   return (
     <TailwindProvider>
-    <SafeAreaView >
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-   </TailwindProvider>
+      <SafeAreaView>
+        <Text className="text-red-600">Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </TailwindProvider>
   );
 }
 
