80436c1b381ae7ba85455f18d249b551bd566aa1
afika <afikashwashwa@gmail.com>
2023-11-20 18:40:01 +0200

    added the necessary media to be utilised

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+// import Landing from './components/Landing'
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up your app!</Text>
+      {/* <Landing/> */}
       <StatusBar style="auto" />
     </View>
   );
