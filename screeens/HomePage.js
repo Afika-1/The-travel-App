74fbe4015a377ccc11cfa0706fd77f01c140dd90
afika <afikashwashwa@gmail.com>
2024-06-19 18:34:49 +0200

    code clean up

--- screeens/HomePage.js
+++ screeens/HomePage.js
@@ -1,30 +1,3 @@
-// import React from 'react'
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View, Image, ImageBackground, SafeAreaView, TouchableOpacity, Button, ScrollView } from 'react-native';
-// import { useFonts } from 'expo-font';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// export default function HomePage({navigation}) {
-//     const [isLoaded] = useFonts({
-//         'Inria-bold': require('../assets/fonts/InriaSerif-Bold.ttf'),
-//         'inria-regular': require('../assets/fonts/InriaSerif-Regular.ttf'),
-//       });
-
-//       if (!isLoaded) {
-//         return null;
-//       }
-
-//   return (
-//     <SafeAreaView>
-//         <View>
-//           <Text style={{marginTop:150}} onPress={() => navigation.navigate('Landing')} >Home</Text>
-//           </View>
-//     </SafeAreaView>
-//   )
-// }
-
-
 // import React from 'react';
 import React, { useState } from 'react';
 import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet, FlatList, SafeAreaView, ScrollView } from 'react-native';
